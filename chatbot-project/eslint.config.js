e769fc5d49b9ee8b0321f84be4b35dbfd1fb6df9
Anshul Sambher <sambher.official@gmail.com>
2025-07-03 01:21:28 +0530

    Set up Chatbot project using ESLint and Vite-provided scaffolding code

--- chatbot-project/eslint.config.js
+++ chatbot-project/eslint.config.js
@@ -2,7 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { FlatCompat } from '@eslint/eslintrc'
 import { defineConfig, globalIgnores } from 'eslint/config'
+
+const compat = new FlatCompat();
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -10,6 +13,7 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      ...compat.extends('plugin:react/recommended'),
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
@@ -22,8 +26,15 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off'
     },
   },
 ])
--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,35 +1,156 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Chatbot } from 'supersimpledev'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function useAutoScroll([ chatMessages ]) {
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const element = elementRef.current;
+    if (element) {
+      element.scrollTop = element.scrollHeight;
+    }
+  }, [ chatMessages ]);
+
+  return elementRef;
+}
+
+function WelcomeMessage() {
+  return (
+    <p
+      className="welcome-message"
+    >Welcome to the chatbot project! Send a message using the textbox below.</p>
+  );
+}
+
+function ChatInput({
+  chatMessages, setChatMessages, isLoading, setIsLoading
+}) {
+  const [ inputText, setInputText ] = useState('');
+
+  function saveInputText(event) {
+    if (event.key === 'Enter') {
+      if (!isLoading && inputText) {
+        sendMessage();
+      }
+    } else if (event.key === 'Escape') {
+      setInputText('');
+    } else {
+      setInputText(event.target.value);
+    }
+  }
+
+  async function sendMessage() {
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: "user",
+        id: crypto.randomUUID()
+      }
+    ];
+
+    setChatMessages(newChatMessages);
+    setInputText('');
+
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: <img src="images/loading-spinner.gif"
+                  className="loading-gif" />,
+        sender: "robot",
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    setIsLoading(true);
+    const response = await Chatbot.getResponseAsync(inputText);
+    setIsLoading(false);
+    
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: "robot",
+        id: crypto.randomUUID()
+      }
+    ]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="chat-input-container">
+      <input 
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        onKeyDown={saveInputText}
+        value={inputText}
+        className="chat-input"
+      />
+      <button
+        onClick={() => {
+          if (!isLoading && inputText) {
+            sendMessage();
+          }
+        }}
+        className="send-button"
+      >Send</button>
+    </div>
+  );
+}
+
+function ChatMessage({ message, sender }) {
+  return (
+    <div className={'chat-message-' + sender}>
+      {sender === 'robot' &&
+      <img src="images/robot.png" className="chat-message-profile" />}
+      <div className="chat-message-text">
+        {message}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {sender === 'user' &&
+      <img src="images/user.png" className="chat-message-profile" />}
+    </div>
+  );
+}
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+
+  return (
+    <div
+      className="chat-messages-container"
+      ref={chatMessagesRef}
+    > {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage 
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function App() {
+  const [ chatMessages, setChatMessages ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  return (
+    <div className="app-container">
+      {!chatMessages.length && <WelcomeMessage />}
+      <ChatMessages
+        chatMessages={chatMessages}
+      />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+      />
+    </div>
+  );
 }
 
 export default App
